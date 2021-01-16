117a3b80d83921cf52be84dbbf02e99297c828ca
Jen Alchimowicz <jedrek.alchimowicz@o2.pl>
2021-01-16 15:40:01 +0000

    m

---
+++ Lecture2/exercise7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+  cout << "Please give the base a:" << endl;
+  cin >> a;
+  cout << "PLease give the exponent n:" << endl;
+  cin >> n;
+  
+}
